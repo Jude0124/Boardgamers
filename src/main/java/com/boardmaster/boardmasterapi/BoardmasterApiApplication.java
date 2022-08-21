33bb5bac03fbe37e25ea5b38abe444d796fc4870
howisitgoing <howisitgoing@kakao.com>
2022-08-21 17:23:53 +0900

    Initial commit

---
+++ src/main/java/com/boardmaster/boardmasterapi/BoardmasterApiApplication.java
@@ -0,0 +1,13 @@
+package com.boardmaster.boardmasterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardmasterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardmasterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boardmaster/boardmasterapi/BoardmasterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardmaster.boardmasterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardmasterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
