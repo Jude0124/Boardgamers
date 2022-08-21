388bda29213ed68470285b2b56f04a1b55cf7e80
howisitgoing <howisitgoing@kakao.com>
2022-08-22 01:40:02 +0900

    회원 테이블 추가

--- src/main/java/com/boardmaster/boardmasterapi/domain/Member.java
+++ src/main/java/com/boardmaster/boardmasterapi/domain/Member.java
@@ -1,4 +1,22 @@
 package com.boardmaster.boardmasterapi.domain;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+    private String userId;
+    private String email;
+    private String password;
+    private String nickname;
+    private String phone;
 }
