0afaa98300bf7d7c27781aeca94ee35225051564
howisitgoing <howisitgoing@kakao.com>
2022-08-22 20:54:07 +0900

    엔티티 구현

--- src/main/java/com/boardmaster/boardmasterapi/domain/Member.java
+++ src/main/java/com/boardmaster/boardmasterapi/domain/Member.java
@@ -1,12 +1,18 @@
 package com.boardmaster.boardmasterapi.domain;
 
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter
 @Table(name = "member")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
@@ -15,8 +21,21 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
     private String userId;
+    @NotNull
     private String email;
     private String password;
     private String nickname;
     private String phone;
+//    @OneToMany(mappedBy = "member")
+//    private List<Article> articles = new ArrayList<>();
+
+    @Builder
+    public Member(Long id, String userId, String email, String password, String nickname, String phone) {
+        this.id = id;
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.phone = phone;
+    }
 }
